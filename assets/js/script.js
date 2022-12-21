4e7d477b1026a990b25359227205e8e2d806d477
melanyhowell <melany.r.howell@gmail.com>
2022-12-21 23:32:57 +0000

    add code for subtract question game

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,10 +31,11 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-    } else if (gameType ==="multiply"){
+    } else if (gameType ==="multiply") {
         displayMultiplyQuestion(num1, num2);
-    }
-    else {
+    } else if (gameType ==="subtract") {
+        displaySubtractQuestion(num1, num2);
+    } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -75,8 +76,9 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x"){
         return [operand1 * operand2, "multiply"];
-    } 
-    else {
+    } else if (operator === "-"){
+        return [operand1 - operand2, "subtract"];
+    } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -108,7 +110,12 @@ function displayAdditionQuestion(operand1, operand2) {
     
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
+    
 
 }
 
