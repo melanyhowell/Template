d2d5af7e299876c5f0a1dc41d1610814ef65c9ab
melanyhowell <melany.r.howell@gmail.com>
2022-12-22 19:20:14 +0000

    amend subtract question

--- assets/js/script.js
+++ assets/js/script.js
@@ -126,7 +126,7 @@ function displayAdditionQuestion(operand1, operand2) {
 function displaySubtractQuestion(operand1, operand2) {
 
     document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById('operand1').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById('operator').textContent = "-";
     
 
@@ -141,7 +141,7 @@ function displayMultiplyQuestion(operand1, operand2) {
     
 }
 
-function displayDivisionQuestion(opearnd1, operand2) {
+function displayDivisionQuestion(operand1, operand2) {
 
     document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById('operand1').textContent = operand1 > operand2 ? operand2 : operand1;
